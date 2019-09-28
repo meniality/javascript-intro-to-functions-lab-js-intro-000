754005919fd87a753d43190385f18fe84ec6d1b8
patrick hoehn <patrh_99@hotmail.com>
2019-09-28 19:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
    return ("I can't hear you!")
- } else {
-   return ("Help")
+ } else if (string === string.toUpperCase()) {
+   return ('YES INDEED!')
+ } else if (string === "I love you, Grandma.") {
+   return "I love you, too."
  }
+
 }
